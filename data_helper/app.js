d55d66fef96d1c7c99d7ab11a91b370cf7ab968a
Wayne Wang <lastr2d2@gmail.com>
2015-06-26 02:58:17 +0800

    create sperate js and css files

---
+++ data_helper/app.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Wayne on 6/26/2015.
+ */
+var app = {
+    init: function () {
+        app.bindUI();
+        app.bindEvents();
+    },
+    bindEvents: function () {
+        app.bindClickEventOnButtonsWithTransformAction();
+        app.bindClickEventOnUndoButton();
+    },
+    bindUI: function () {
+        app.applyLinedTextareaToContainer();
+    },
+    bindClickEventOnButtonsWithTransformAction: function(){
+        $('.btn-with-transform-action').click(function(event){
+            var $target = $(event.target);
+            var action = app.methods[$target.data('transform')];
+            if (typeof action === "function")
+                app.applyTransform(action);
+        });
+    },
+    bindClickEventOnUndoButton:function(){
+        $('#Undo').click(function () {
+            var history = $('#content').data('history');
+            if (history) {
+                var content = history.pop();
+                $('#content').val(content);
+            }
+        });
+    },
+    applyLinedTextareaToContainer:function(){
+        $("#content").linedtextarea();
+    },
+    applyTransform:function(action){
+        var content = $('#content').val();
+        var history = $('#content').data('history');
+        if (!history) {
+            history = new Array();
+        }
+        history.push(content);
+
+        if (history.length > 40) {
+            history = history.shift();
+        }
+
+        $('#content').data('history', history);
+
+        $('#content').val(action(content));
+    },
+    methods: {
+        removeEmptyLine: function (text) {
+            return text.replace(/^\s*\n/gm, '');
+        },
+        createSqlInClause:function(text){
+            var lines = text.split('\n');
+            return lines.map(function (element) {
+                return "'" + element + "',"
+            }).join('\n').replace(/,+$/, "");
+        },
+        createSqlValuesClause:function(text){
+            var lines = text.split('\n');
+            var result = lines.map(function (element) {
+                return "('" + element.replace(/\t/gm, "','") + "')";
+            }).join('\n');
+            return result;
+        },
+        createSqlSelectClause:function(text){
+            var lines = text.split('\n');
+            var result = lines.map(function (element) {
+                return '[' + element.replace(/\t/gm, "],[") + ']';
+            }).join('\n');
+            return result;
+        },
+        toPrettyJSON:function(text){
+            return JSON.stringify(JSON.parse(text), undefined, 4);
+        },
+        toPrettyXml:function(text){
+            return vkbeautify.xml(text);
+        }
+    }
+};
+(function () {
+    $(document).ready(function(){
+        app.init();
+    });
+})();
